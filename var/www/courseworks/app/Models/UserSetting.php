99ad3a7674b7dbbef3a3054f24581bd0012b3ff2
ignat <ignatov.d43@gmail.com>
2024-01-08 12:33:14 +0300

    Added ide helper

--- var/www/courseworks/app/Models/UserSetting.php
+++ var/www/courseworks/app/Models/UserSetting.php
@@ -5,6 +5,16 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\UserSetting
+ *
+ * @property int $id
+ * @method static \Illuminate\Database\Eloquent\Builder|UserSetting newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|UserSetting newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|UserSetting query()
+ * @method static \Illuminate\Database\Eloquent\Builder|UserSetting whereId($value)
+ * @mixin \Eloquent
+ */
 class UserSetting extends Model
 {
     use HasFactory;
